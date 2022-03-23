01ba32913543fe9f49d76fd764a370185bee1a65
ShihaoXiong <shihao.xiong@icloud.com>
2022-03-23 17:55:38 -0400

    feat: upload file to GCS

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const configRoutes = require('./routes');
-// const bodyParser = require('body-parser')
 const multer = require('multer');
 
 const multerMid = multer({
@@ -13,13 +12,10 @@ const multerMid = multer({
 });
 
 app.use(express.json());
-// app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
 
 app.disable('x-powered-by');
 app.use(multerMid.single('file'));
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: false}))
 app.use(
 	session({
 		name: 'AuthCookie',
