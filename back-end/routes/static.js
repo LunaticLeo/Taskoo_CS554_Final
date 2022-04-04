4d0b8b89caf9aabd6687ce2a05ca9f01ee109354
ShihaoXiong <shihao.xiong@icloud.com>
2022-04-04 16:08:35 -0400

    ci: cache the static in redis

--- back-end/routes/static.js
+++ back-end/routes/static.js
@@ -1,8 +1,9 @@
+const client = require('../utils/redis');
 const router = require('express').Router();
 const { getStaticData } = require('../data/static');
 const Check = require('../lib/Check');
 
-router.get('/:collection', async (req, res) => {
+router.get('/:collection', async (req, res, next) => {
 	const { collection } = req.params;
 	const { id } = req.query;
 
@@ -14,7 +15,20 @@ router.get('/:collection', async (req, res) => {
 	}
 
 	try {
+		const redisData = await client.get(collection);
+		redisData ? res.json({ code: 200, message: '', data: JSON.parse(staticData) }) : next();
+	} catch (error) {
+		res.status(500).json({ code: 400, message: error?.message ?? error });
+	}
+});
+
+router.get('/:collection', async (req, res) => {
+	const { collection } = req.params;
+	const { id } = req.query;
+
+	try {
 		const staticData = await getStaticData(collection, id);
+		!id && (await client.set(collection, JSON.stringify(staticData)));
 		res.json({ code: 200, message: '', data: staticData });
 	} catch (error) {
 		res.status(500).json({ code: 500, message: error?.message ?? error });
