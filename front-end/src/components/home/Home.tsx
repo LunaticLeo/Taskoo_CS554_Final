aa8f15ff0defdaae0ea5cbf6e0a48319cdabff88
liaoyufu-leo <liaoyufu.leo@gmail.com>
2022-04-04 21:33:54 +0000

    feat: complete view of calendar in home

--- front-end/src/components/home/Home.tsx
+++ front-end/src/components/home/Home.tsx
@@ -5,6 +5,7 @@ import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import Dashboard from './Dashboard';
 import Nav from './Nav';
 import { toCapitalize } from '@/utils';
+import Calendar from './Calendar';
 
 const Home: React.FC = () => {
 	const [openDrawer, setOpenDrawer] = useState<boolean>(false);
@@ -17,7 +18,7 @@ const Home: React.FC = () => {
 	return (
 		<Box sx={{ display: 'flex', height: '100vh' }}>
 			<Nav openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
-			<Paper component='main' square elevation={0} sx={{flex: 1}}>
+			<Paper component='main' square elevation={0} sx={{ flex: 1 }}>
 				<Toolbar>
 					<IconButton
 						aria-label='drawer-control'
@@ -33,6 +34,7 @@ const Home: React.FC = () => {
 				<Routes>
 					<Route path='/' element={<Navigate to='/home/dashboard' replace />} />
 					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/calendar' element={<Calendar />} />
 				</Routes>
 			</Paper>
 		</Box>
