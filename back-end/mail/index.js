765d905494d7e979763c5bbcb09b64e42f4927db
liaoyufu-leo <liaoyufu.leo@gmail.com>
2022-04-04 13:49:27 -0400

    fix: comment mail

--- back-end/mail/index.js
+++ back-end/mail/index.js
@@ -2,16 +2,16 @@ const nodemailer = require('nodemailer');
 const config = require('../utils/mail.json');
 const transporter = nodemailer.createTransport(config);
 
-mail = {
-    from: 'taskoo.cs554final@gmail.com',
-    to: 'yliao10@stevens.edu',
-    subject: 'Message title',
-    text: 'content'
-};
+// mail = {
+//     from: 'taskoo.cs554final@gmail.com',
+//     to: 'yliao10@stevens.edu',
+//     subject: 'Message title',
+//     text: 'content'
+// };
 
 // nodemailer API: https://nodemailer.com/about/
 
-function sendMail(mail){
+function sendMail(mail) {
     transporter.sendMail(mail, function (error, info) {
         if (error) {
             console.log(error);
