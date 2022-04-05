3c3b5d9ef46022d72c118fdd67152bd82643cf23
ShihaoXiong <shihao.xiong@icloud.com>
2022-04-04 20:43:19 -0400

    refactor: static api

--- back-end/data/static.js
+++ back-end/data/static.js
@@ -1,21 +1,29 @@
+const client = require('../utils/redis');
 const mongoCollections = require('../config/mongoCollections');
 const Check = require('../lib/Check');
 
 /**
  * get the static data from collection
  * @param {string} collectionName collection name in DB
- * @param {string} _id optional (if provide, return the specific static data otherwise return the whole list)
+ * @param {string} _id (optional) if provide, return the specific static data otherwise return the whole list
  * @returns {DBStaticCollection | DBStaticCollection[]}
  */
 const getStaticData = async (collectionName, _id) => {
-	const collection = await mongoCollections[collectionName]();
-
 	let staticData;
+
 	if (_id) {
 		Check._id(_id);
+		const collection = await mongoCollections[collectionName]();
 		staticData = await collection.findOne({ _id });
 	} else {
-		staticData = await collection.find({}).toArray();
+		const redisData = await client.get(collectionName);
+		if (!redisData) {
+			const collection = await mongoCollections[collectionName]();
+			staticData = await collection.find({}).toArray();
+			await client.set(collectionName, JSON.stringify(staticData));
+		} else {
+			staticData = JSON.parse(redisData);
+		}
 	}
 
 	return staticData;
--- back-end/routes/static.js
+++ back-end/routes/static.js
@@ -1,9 +1,8 @@
-const client = require('../utils/redis');
 const router = require('express').Router();
 const { getStaticData } = require('../data/static');
 const Check = require('../lib/Check');
 
-router.get('/:collection', async (req, res, next) => {
+router.get('/:collection', async (req, res) => {
 	const { collection } = req.params;
 	const { id } = req.query;
 
@@ -15,24 +14,7 @@ router.get('/:collection', async (req, res, next) => {
 	}
 
 	try {
-		if (id) {
-			next();
-		} else {
-			const redisData = await client.get(collection);
-			redisData ? res.json({ code: 200, message: '', data: JSON.parse(redisData) }) : next();
-		}
-	} catch (error) {
-		res.status(500).json({ code: 500, message: error?.message ?? error });
-	}
-});
-
-router.get('/:collection', async (req, res) => {
-	const { collection } = req.params;
-	const { id } = req.query;
-
-	try {
 		const staticData = await getStaticData(collection, id);
-		!id && (await client.set(collection, JSON.stringify(staticData)));
 		res.json({ code: 200, message: '', data: staticData });
 	} catch (error) {
 		res.status(500).json({ code: 500, message: error?.message ?? error });
