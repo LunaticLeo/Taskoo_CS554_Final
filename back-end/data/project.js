f77e006d8137e8b399adbff09434bb9a62166587
ShihaoXiong <shihao.xiong@icloud.com>
2022-04-07 18:14:25 -0400

    ci: optimize route (/project/create)

--- back-end/data/project.js
+++ back-end/data/project.js
@@ -1,15 +1,22 @@
 const { Project } = require('../lib');
-const { project } = require('../config/mongoCollections');
+const { project, bucket } = require('../config/mongoCollections');
 
 /**
  * create project
  * @param {Project} projectObj
  */
-const createProject = async projectObj => {
+const createProject = async (projectObj, bucketId) => {
 	const newProject = new Project(projectObj);
 
-	const collection = await project();
-	const { insertedId } = await collection.insertOne(newProject);
+	const peojectCol = await project();
+	const { insertedId } = await peojectCol.insertOne(newProject);
+
+	// update bucket
+	const bucketCol = await bucket();
+	const { projects } = await bucketCol.findOne({ _id: bucketId }, { projection: { projects: 1 } });
+	projects[newProject.status.toLowerCase()].push(newProject._id);
+	await bucketCol.updateOne({ _id: bucketId }, { $set: { projects } });
+
 	return `Project ${newProject.name} (id: ${insertedId}) create successfully`;
 };
 
--- back-end/routes/project.js
+++ back-end/routes/project.js
@@ -4,15 +4,17 @@ const { Project } = require('../lib');
 const router = require('express').Router();
 
 router.post('/create', async (req, res) => {
+	const { _id, bucket } = req.session.accountInfo;
+
 	let newProject;
 	try {
-		newProject = new Project({ ...req.body, manager: req.session.accountInfo._id });
+		newProject = new Project({ ...req.body, manager: _id });
 	} catch (error) {
 		return res.status(400).json({ code: 400, message: error?.message ?? error });
 	}
 
 	try {
-		const message = await createProject(newProject);
+		const message = await createProject(newProject, bucket);
 		res.json({ code: 200, message });
 	} catch (error) {
 		res.status(500).json({ code: 500, message: error?.message ?? error });
