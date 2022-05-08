b3d57f5f0a8e5db3e76b5dbe0b476c4cbecad75b
wjzhou14 <18679699872@163.com>
2022-05-07 22:53:22 -0400

    no message

--- front-end/src/hooks/useValidation.ts
+++ front-end/src/hooks/useValidation.ts
@@ -54,15 +54,38 @@ const useValidation = () => {
 					switchHelperText(1, 'The password cannot be empty.');
 					return;
 				}
-				// TODO complete password validation
-				// const reg = /^[0-9a-zA-Z_.-]+[@][0-9a-zA-Z_.-]+([.][a-zA-Z]+){1,2}$/;
-				// if (!reg.test(e.target.value)) {
-				// 	switchError(0, true);
-				// 	switchHelperText(0, 'The emial is invalid.');
-				// 	return;
-				// }
-				switchError(1, false);
-				switchHelperText(1, ' ');
+				const isAlphanumeric = /^[0-9a-zA-Z]*$/
+				const number = /[0-9]/; 
+				const alphabet = /[a-z]/i
+				if(value.length<8||value.length>16) 
+				{
+					switchError(1, true);
+					switchHelperText(1, "Password's length is 8 to 16");
+					return
+				}
+				if(isAlphanumeric.test(value))
+				{
+					if(number.test(value))
+					{
+						if(!alphabet.test(value))
+						{
+							switchError(1, true);
+							switchHelperText(1, "Password must have letter");
+							return
+						}
+					}else{
+						switchError(1, true);
+						switchHelperText(1, "Password must have number");
+						return
+					}
+				}else{
+					switchError(1, true);
+					switchHelperText(1, "Password can only contain numbers and letters");
+					return
+				}
+				
+				switchError(0, false);
+				switchHelperText(0, ' ');
 			}
 		}),
 		valid:(cb?: any) => ({
